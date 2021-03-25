a779e6dcef5001ec9c61100863e6664de708b655
titan00001 <gplay9792@gmail.com>
2021-03-25 20:50:59 +0530

    test: remove time field

--- backend/src/proxy/transaction.js
+++ backend/src/proxy/transaction.js
@@ -114,16 +114,5 @@ async function updateTransaction(transaction, id) {
   }
 }
 
-// function errorHandler(err) {
-//   if (err instanceof Error) {
-//     console.log(err);
-//     throw { code: 500, message: err };
-//   } else if (err.code) {
-//     throw err;
-//   } else {
-//     // if someone's dog got lost or any other esoteric errors
-//     throw { code: 500, message: err };
-//   }
-// }
 
 module.exports = TransactionDao;
