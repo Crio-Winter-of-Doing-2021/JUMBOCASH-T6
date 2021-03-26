bdf2032369a3ce8bd8119273b9b38f1f282d714e
titan00001 <gplay9792@gmail.com>
2021-03-26 20:16:05 +0530

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 const sequelize = require('./config/database');
 const keys = require('./config/server');
-// require('./src/models/index');
-
-// require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,7 +21,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
